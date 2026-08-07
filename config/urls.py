e88109db25a18446cb1ab9f1682b3a1889c5bef1
Tradition Learning Community <traditionlearningcommunity@gmail.com>
2026-08-07 18:10:53 +0200

    feat(events): wire events web and API routes

--- config/urls.py
+++ config/urls.py
@@ -1,20 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
 
 urlpatterns = [
-
-    path(
-        'admin/',
-        admin.site.urls
-    ),
-
-    path(
-        'api/v1/accounts/',
-        include('accounts.api.urls')
-    ),
-
-    path(
-        '',
-        include('core.urls')
-    ),
+    path("admin/", admin.site.urls),
+    path("api/v1/accounts/", include("accounts.api.urls")),
+    path("api/v1/events/", include("events.api.urls")),
+    path("events/", include("events.urls")),
+    path("", include("core.urls")),
 ]
