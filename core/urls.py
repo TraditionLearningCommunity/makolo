0adc3e92fee1600a3c08662535e3b0b37ee54b92
Tradition Learning Community <traditionlearningcommunity@gmail.com>
2026-08-07 15:53:36 +0200

    feat: add web login and logout routes

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,24 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+
 from .views import DashboardView
 
+
+app_name = "core"
+
 urlpatterns = [
+    path(
+        "login/",
+        LoginView.as_view(
+            template_name="auth/login.html",
+            redirect_authenticated_user=True,
+        ),
+        name="login",
+    ),
+    path(
+        "logout/",
+        LogoutView.as_view(next_page="core:login"),
+        name="logout",
+    ),
     path("", DashboardView.as_view(), name="dashboard"),
 ]
