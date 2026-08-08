c6eee514532ee75d480bb51e894a510783d966e5
Tradition Learning Community <traditionlearningcommunity@gmail.com>
2026-08-08 21:47:17 +0200

    feat: register partner order lifecycle signals

--- partners/apps.py
+++ partners/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class PartnersConfig(AppConfig):
-    name = 'partners'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "partners"
+
+    def ready(self):
+        from . import signals  # noqa: F401
