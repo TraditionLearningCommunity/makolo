f41011395149c468e2ca6a2a70696be435921139
Tradition Learning Community <traditionlearningcommunity@gmail.com>
2026-08-08 13:15:45 +0200

    feat(notifications): register web and API routes

--- config/urls.py
+++ config/urls.py
@@ -11,10 +11,12 @@ urlpatterns = [
     path("api/v1/tickets/", include("tickets.api.urls")),
     path("api/v1/scanner/", include("scanner.api.urls")),
     path("api/v1/payments/", include("payments.api.urls")),
+    path("api/v1/notifications/", include("notifications.api.urls")),
     path("events/", include("events.urls")),
     path("tickets/", include("tickets.urls")),
     path("scanner/", include("scanner.urls")),
     path("payments/", include("payments.urls")),
+    path("notifications/", include("notifications.urls")),
     path("", include("core.urls")),
 ]
 
