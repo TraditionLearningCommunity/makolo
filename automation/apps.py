7ecf2cb176b741d9567cea7ceb6573c3fa0b1a7b
Tradition Learning Community <traditionlearningcommunity@gmail.com>
2026-08-09 05:47:21 +0200

    feat: register CRM workflow signals

--- automation/apps.py
+++ automation/apps.py
@@ -5,3 +5,6 @@ class AutomationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "automation"
     verbose_name = "Makolo Autopilot"
+
+    def ready(self):
+        from . import crm_signals  # noqa: F401
