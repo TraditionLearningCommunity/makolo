e4c9be44f661450e03a618b2fe972e91e6cd4000
Tradition Learning Community <traditionlearningcommunity@gmail.com>
2026-08-10 00:06:59 +0200

    feat(tickets): migrate participant ticket visibility

--- tickets/migrations/0004_ticketorder_idempotency.py
+++ tickets/migrations/0004_ticketorder_idempotency.py
@@ -9,6 +9,14 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
+            model_name="tickettype",
+            name="is_public",
+            field=models.BooleanField(
+                default=True,
+                help_text="Visible et achetable depuis les parcours participants publics.",
+            ),
+        ),
+        migrations.AddField(
             model_name="ticketorder",
             name="idempotency_fingerprint",
             field=models.CharField(blank=True, editable=False, max_length=64),
