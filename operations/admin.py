3583d3d8fb857b99cda6fe5287957a335ced0963
Tradition Learning Community <traditionlearningcommunity@gmail.com>
2026-08-09 08:43:41 +0200

    fix: make worker heartbeat admin immutable

--- operations/admin.py
+++ operations/admin.py
@@ -68,3 +68,9 @@ class WorkerHeartbeatAdmin(admin.ModelAdmin):
 
     def has_add_permission(self, request):
         return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
