f4aadc2caa137d71e0cbf7833a11927171cb9314
Tradition Learning Community <traditionlearningcommunity@gmail.com>
2026-08-10 16:18:04 +0200

    feat: orchestrate partners loyalty and analytics demo data

---
+++ demo_seed/partners_loyalty.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from .analytics_demo import _seed_growth_spend
+from .common import SeedContext
+from .loyalty_demo import _seed_loyalty
+from .partners_demo import _seed_partners
+
+
+def seed_partners_loyalty_and_analytics(ctx: SeedContext) -> None:
+    _seed_partners(ctx)
+    _seed_loyalty(ctx)
+    _seed_growth_spend(ctx)
