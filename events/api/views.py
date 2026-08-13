865284e7a79f896a2250f0c40f47d257c09dad4f
Tradition Learning Community <traditionlearningcommunity@gmail.com>
2026-08-13 19:45:57 +0200

    refactor: project Event API writes into Activity core

--- events/api/views.py
+++ events/api/views.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError as DjangoValidationError
+from django.db import transaction
 
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
@@ -7,6 +8,7 @@ from rest_framework.parsers import FormParser, JSONParser, MultiPartParser
 from rest_framework.response import Response
 
 from automation.services import ensure_policy
+from events.activity_bridge import sync_event_core
 from events.models import EventCategory, EventStatus, EventVenue
 from events.permissions import IsEventOrganizer, IsEventOwnerOrAdmin
 from events.selectors import get_events_visible_to
@@ -35,31 +37,30 @@ class EventViewSet(viewsets.ModelViewSet):
     http_method_names = ["get", "post", "patch", "delete", "head", "options"]
 
     def get_queryset(self):
-        return get_events_visible_to(
-            self.request.user,
-            for_detail=self.action in {"retrieve", "partial_update", "destroy", "publish", "cancel", "complete"},
-        )
+        return get_events_visible_to(self.request.user, for_detail=self.action in {"retrieve", "partial_update", "destroy", "publish", "cancel", "complete"})
 
     def get_serializer_class(self):
-        if self.action == "list":
-            return EventListSerializer
-        if self.action in {"create", "partial_update"}:
-            return EventWriteSerializer
+        if self.action == "list": return EventListSerializer
+        if self.action in {"create", "partial_update"}: return EventWriteSerializer
         return EventDetailSerializer
 
     def get_permissions(self):
-        if self.action in {"list", "retrieve"}:
-            classes = [permissions.AllowAny]
-        elif self.action == "create":
-            classes = [IsEventOrganizer]
-        else:
-            classes = [IsEventOwnerOrAdmin]
+        if self.action in {"list", "retrieve"}: classes = [permissions.AllowAny]
+        elif self.action == "create": classes = [IsEventOrganizer]
+        else: classes = [IsEventOwnerOrAdmin]
         return [permission() for permission in classes]
 
     def perform_create(self, serializer):
-        organization = serializer.validated_data.get("organization") or ensure_personal_organization(self.request.user)
-        event = serializer.save(organizer=self.request.user, organization=organization)
-        ensure_policy(event)
+        with transaction.atomic():
+            organization = serializer.validated_data.get("organization") or ensure_personal_organization(self.request.user)
+            event = serializer.save(organizer=self.request.user, organization=organization)
+            sync_event_core(event)
+            ensure_policy(event)
+
+    def perform_update(self, serializer):
+        with transaction.atomic():
+            event = serializer.save()
+            sync_event_core(event)
 
     def destroy(self, request, *args, **kwargs):
         event = self.get_object()
@@ -69,22 +70,16 @@ class EventViewSet(viewsets.ModelViewSet):
         return super().destroy(request, *args, **kwargs)
 
     def _transition(self, request, service):
-        event = self.get_object()
-        self.check_object_permissions(request, event)
-        try:
-            service(event=event, actor=request.user)
-        except DjangoValidationError as exc:
-            raise ValidationError(exc.messages) from exc
+        event = self.get_object(); self.check_object_permissions(request, event)
+        try: service(event=event, actor=request.user)
+        except DjangoValidationError as exc: raise ValidationError(exc.messages) from exc
         return Response(EventDetailSerializer(event, context={"request": request}).data, status=status.HTTP_200_OK)
 
     @action(detail=True, methods=["post"])
-    def publish(self, request, slug=None):
-        return self._transition(request, publish_event)
+    def publish(self, request, slug=None): return self._transition(request, publish_event)
 
     @action(detail=True, methods=["post"])
-    def cancel(self, request, slug=None):
-        return self._transition(request, cancel_event)
+    def cancel(self, request, slug=None): return self._transition(request, cancel_event)
 
     @action(detail=True, methods=["post"])
-    def complete(self, request, slug=None):
-        return self._transition(request, complete_event)
+    def complete(self, request, slug=None): return self._transition(request, complete_event)
