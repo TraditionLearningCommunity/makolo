0c8daec272050e5db4f37db3986cfdf1ab77c3e6
Tradition Learning Community <traditionlearningcommunity@gmail.com>
2026-08-14 15:00:33 +0200

    refactor: activate Ticket Journey Access bridge

--- tickets/apps.py
+++ tickets/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class TicketsConfig(AppConfig):
-    name = 'tickets'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tickets"
+
+    def ready(self):
+        from . import journey_access_bridge  # noqa: F401
