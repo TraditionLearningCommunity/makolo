a62f7dee7746fb4ecfced7f271704780562c472c
Tradition Learning Community <traditionlearningcommunity@gmail.com>
2026-08-14 15:33:52 +0200

    fix: activate Access credential legacy invalidation hook

--- access/apps.py
+++ access/apps.py
@@ -5,3 +5,6 @@ class AccessConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "access"
     verbose_name = "Accès"
+
+    def ready(self):
+        from . import signals  # noqa: F401
