bfb7a4b7090b543bbd34f8a2cd9e2b628feda2aa
Gilbert243 <gilbert.bemwiz@fitforpurpose.tech>
2026-08-18 15:38:49 +0200

    fix: activate Event cutover compatibility adapter

--- events/apps.py
+++ events/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class EventsConfig(AppConfig):
-    name = 'events'
+    name = "events"
+
+    def ready(self):
+        from .legacy_compat import install_event_legacy_compat
+
+        install_event_legacy_compat()
