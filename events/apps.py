661a38b42cfb1c04be992266e022238602682ba0
Gilbert243 <gilbert.bemwiz@fitforpurpose.tech>
2026-08-18 15:42:16 +0200

    fix: activate scoped canonical ORM aliases

--- events/apps.py
+++ events/apps.py
@@ -6,5 +6,7 @@ class EventsConfig(AppConfig):
 
     def ready(self):
         from .legacy_compat import install_event_legacy_compat
+        from .orm_compat import install_orm_path_compat
 
         install_event_legacy_compat()
+        install_orm_path_compat()
