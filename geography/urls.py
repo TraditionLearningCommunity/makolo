62f0781d06060d7067f74ecc01ff9c493224ce6b
Gilbert243 <gilbert.bemwiz@fitforpurpose.tech>
2026-08-19 18:20:19 +0200

    refactor: keep Place mutations behind Space Console

--- geography/urls.py
+++ geography/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import SpacePlaceCreateView, SpacePlaceDeactivateView, SpacePlaceListView, SpacePlaceUpdateView
+from .views import SpacePlaceCreateView, SpacePlaceDeactivateView, SpacePlaceUpdateView
 
 
 urlpatterns = [
-    path("<slug:slug>/places/", SpacePlaceListView.as_view(), name="places"),
     path("<slug:slug>/places/new/", SpacePlaceCreateView.as_view(), name="place-create"),
     path("<slug:slug>/places/<uuid:pk>/edit/", SpacePlaceUpdateView.as_view(), name="place-relation-edit"),
     path("<slug:slug>/places/<uuid:pk>/deactivate/", SpacePlaceDeactivateView.as_view(), name="place-relation-deactivate"),
