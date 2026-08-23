cd87e6ab28fdcbd09d42086725b4fb47a2279e94
Gilbert243 <gilbert.bemwiz@fitforpurpose.tech>
2026-08-23 21:23:26 +0200

    Task 19: add responsibility forms

---
+++ organizations/team_forms.py
@@ -0,0 +1,62 @@
+from django import forms
+
+from activities.models import Activity
+from authorization.constants import STANDARD_ACTIVITY_ROLE_CODES, STANDARD_SPACE_ROLE_CODES, SystemRoleCode
+from authorization.models import AuthorityScope, Role
+from authorization.services import can
+from authorization.constants import PermissionCode
+
+from .forms import INPUT_CLASS
+
+
+class MemberSpaceResponsibilityForm(forms.Form):
+    role = forms.ChoiceField(
+        label="Responsabilité dans l'Espace",
+        help_text="Choisissez la responsabilité générale de cette personne dans cet Espace.",
+    )
+
+    def __init__(self, *args, actor, space, current_role_code=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        can_manage_ownership = can(actor, PermissionCode.SPACE_OWNERSHIP_MANAGE, space)
+        roles = list(
+            Role.objects.filter(
+                scope_type=AuthorityScope.SPACE,
+                is_system=True,
+                is_active=True,
+                code__in=STANDARD_SPACE_ROLE_CODES,
+            ).order_by("name", "code")
+        )
+        choices = []
+        for role in roles:
+            if role.code == SystemRoleCode.SPACE_OWNER and not can_manage_ownership and current_role_code != role.code:
+                continue
+            choices.append((role.code, role.name))
+        self.fields["role"].choices = choices
+        self.fields["role"].widget.attrs["class"] = INPUT_CLASS
+        if current_role_code:
+            self.fields["role"].initial = current_role_code
+
+
+class MemberActivityResponsibilityForm(forms.Form):
+    activity = forms.ModelChoiceField(
+        queryset=Activity.objects.none(),
+        label="Activité",
+        help_text="Seules les activités de cet Espace peuvent être ciblées.",
+    )
+    role = forms.ChoiceField(
+        label="Responsabilité",
+        help_text="Cette responsabilité sera limitée à l'activité choisie.",
+    )
+
+    def __init__(self, *args, space, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["activity"].queryset = Activity.objects.filter(space=space).order_by("title", "pk")
+        roles = Role.objects.filter(
+            scope_type=AuthorityScope.ACTIVITY,
+            is_system=True,
+            is_active=True,
+            code__in=STANDARD_ACTIVITY_ROLE_CODES,
+        ).order_by("name", "code")
+        self.fields["role"].choices = [(role.code, role.name) for role in roles]
+        for field in self.fields.values():
+            field.widget.attrs["class"] = INPUT_CLASS
