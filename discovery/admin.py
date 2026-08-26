7fd82794d7253e7ea57b16eb8f61bed0fad16098
Gilbert243 <gilbert.bemwiz@fitforpurpose.tech>
2026-08-26 13:02:44 +0200

    Task 26: administer Activity bookmarks

--- discovery/admin.py
+++ discovery/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from .models import EventBookmark
+from .models import ActivityBookmark
 
 
-@admin.register(EventBookmark)
-class EventBookmarkAdmin(admin.ModelAdmin):
-    list_display = ("user", "event", "created_at")
-    search_fields = ("user__email", "event__title")
-    raw_id_fields = ("user", "event")
+@admin.register(ActivityBookmark)
+class ActivityBookmarkAdmin(admin.ModelAdmin):
+    list_display = ("user", "activity", "created_at")
+    search_fields = ("user__email", "activity__title")
+    raw_id_fields = ("user", "activity")
     readonly_fields = ("created_at",)
