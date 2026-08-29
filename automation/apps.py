e56af382736f782e8051932dd4e7e5bf29840bd3
Gilbert243 <gilbert.bemwiz@fitforpurpose.tech>
2026-08-29 16:34:50 +0200

    T34B: install additive Services Automation contracts

--- automation/apps.py
+++ automation/apps.py
@@ -6,5 +6,8 @@ class AutomationConfig(AppConfig):
     name = "automation"
 
     def ready(self):
+        from .service_contracts import install_service_automation_contracts
+
+        install_service_automation_contracts()
         from . import crm_signals  # noqa: F401
         from . import domain_event_consumer  # noqa: F401
