03b89a3e79be72e84643717ab4341e930d6cdb4b
Gilbert243 <gilbert.bemwiz@fitforpurpose.tech>
2026-08-30 23:32:03 +0200

    S5: register subscription notification consumer

--- notifications/apps.py
+++ notifications/apps.py
@@ -8,3 +8,4 @@ class NotificationsConfig(AppConfig):
     def ready(self):
         from . import domain_event_consumer  # noqa: F401
         from . import services_domain_event_consumer  # noqa: F401
+        from . import subscriptions_domain_event_consumer  # noqa: F401
