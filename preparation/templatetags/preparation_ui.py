c23dd557f40ea0ce34b8834e2bc47bebb23903f7
Gilbert243 <gilbert.bemwiz@fitforpurpose.tech>
2026-09-01 19:00:08 +0200

    M2: compose participant preparation workspace

---
+++ preparation/templatetags/preparation_ui.py
@@ -0,0 +1,20 @@
+from django import template
+
+from preparation.services import resources_for_journey
+
+
+register = template.Library()
+
+
+@register.inclusion_tag("preparation/participant_workspace.html", takes_context=True)
+def participant_preparation_workspace(context, journey):
+    actor = context["request"].user
+    form_requests = list(
+        journey.form_requests.select_related("form_version", "form_version__form", "response").order_by("created_at", "id")
+    )
+    resources = resources_for_journey(journey=journey, actor=actor)
+    return {
+        "journey": journey,
+        "form_requests": form_requests,
+        "resources": resources,
+    }
