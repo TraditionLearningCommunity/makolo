be3bbb210fb573674aca6cb432c676b053091356
Gilbert243 <gilbert.bemwiz@fitforpurpose.tech>
2026-09-01 19:07:34 +0200

    M2: reuse readiness form prefetch in participant workspace

--- preparation/templatetags/preparation_ui.py
+++ preparation/templatetags/preparation_ui.py
@@ -9,9 +9,7 @@ register = template.Library()
 @register.inclusion_tag("preparation/participant_workspace.html", takes_context=True)
 def participant_preparation_workspace(context, journey):
     actor = context["request"].user
-    form_requests = list(
-        journey.form_requests.select_related("form_version", "form_version__form", "response").order_by("created_at", "id")
-    )
+    form_requests = list(journey.form_requests.all())
     resources = resources_for_journey(journey=journey, actor=actor)
     return {
         "journey": journey,
