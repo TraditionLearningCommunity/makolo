df6dcd7a094dc3557e55810b0fda6808a66912e1
Gilbert243 <gilbert.bemwiz@fitforpurpose.tech>
2026-09-05 01:13:00 +0200

    G7 expose bilateral primitives in admin

--- social/admin.py
+++ social/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Contribution
+from .models import ActionNeed, Contribution, ProfileSolicitation
 
 
 @admin.register(Contribution)
@@ -9,3 +9,19 @@ class ContributionAdmin(admin.ModelAdmin):
     list_filter = ("kind", "visibility", "status")
     search_fields = ("author_profile__email", "author_profile__username", "body")
     readonly_fields = ("created_at", "updated_at", "edited_at", "moderated_at")
+
+
+@admin.register(ActionNeed)
+class ActionNeedAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner_profile", "space", "open_to_kind", "status", "created_by", "created_at")
+    list_filter = ("status", "open_to_kind")
+    search_fields = ("title", "description", "owner_profile__username", "space__name")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(ProfileSolicitation)
+class ProfileSolicitationAdmin(admin.ModelAdmin):
+    list_display = ("need", "recipient_profile", "sent_by", "status", "created_at")
+    list_filter = ("status",)
+    search_fields = ("need__title", "recipient_profile__username", "sent_by__username")
+    readonly_fields = ("created_at", "updated_at", "responded_at", "cancelled_at")
