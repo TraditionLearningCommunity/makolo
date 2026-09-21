37f8745ad53dfde47956d8ed759406a0df21bf2e
Gilbert243 <gilbert.bemwiz@fitforpurpose.tech>
2026-09-21 12:48:30 +0200

    Z4 compose Moi routes with merged Z2 namespace

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,11 +1,45 @@
 from django.urls import path
 
+from .me_views import (
+    PersonalCollectivesAPIView,
+    PersonalConsiderationsAPIView,
+    PersonalMeAPIView,
+    PersonalPartnersAPIView,
+    PersonalPassportAPIView,
+    PersonalResourcesAPIView,
+)
 from .personal_views import PersonalNowAPIView, PersonalOngoingAPIView
 
 
 app_name = "personal-projections"
 
 urlpatterns = [
+    path("", PersonalMeAPIView.as_view(), name="me"),
     path("now/", PersonalNowAPIView.as_view(), name="now"),
     path("ongoing/", PersonalOngoingAPIView.as_view(), name="ongoing"),
+    path(
+        "considerations/",
+        PersonalConsiderationsAPIView.as_view(),
+        name="considerations",
+    ),
+    path(
+        "collectives/",
+        PersonalCollectivesAPIView.as_view(),
+        name="collectives",
+    ),
+    path(
+        "passport/",
+        PersonalPassportAPIView.as_view(),
+        name="passport",
+    ),
+    path(
+        "resources/",
+        PersonalResourcesAPIView.as_view(),
+        name="resources",
+    ),
+    path(
+        "partners/",
+        PersonalPartnersAPIView.as_view(),
+        name="partners",
+    ),
 ]
