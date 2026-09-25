c2d44e4dc0e558ed32ceefa7aab9f2b3def8b242
Gilbert243 <gilbert.bemwiz@fitforpurpose.tech>
2026-09-25 23:35:01 +0200

    feat(z15): expose platform capability API

--- config/urls.py
+++ config/urls.py
@@ -43,6 +43,7 @@ urlpatterns = [
     path("api/v1/social/", include("social.api_urls")),
     path("api/v1/goals/", include("goals.api_urls")),
     path("api/v1/growth/", include("growth.api.urls")),
+    path("api/v1/platform/", include("core.api.platform_urls")),
     path("api/v1/spatiotemporal/", include("spatiotemporal.api_urls")),
     path("g/", include("growth.public_urls")),
     path("o/", include("organizations.public_urls")),
