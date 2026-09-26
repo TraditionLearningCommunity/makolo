fee07c0153b5b075b2b060ef9dcf9f2523f5e877
Gilbert243 <gilbert.bemwiz@fitforpurpose.tech>
2026-09-26 12:17:01 +0200

    mobile(a1): wire startup and canonical navigation

---
+++ mobile/lib/features/splash/splash_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../../design/makolo_mark.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) => const Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: MakoloMark(size: 72),
+          ),
+        ),
+      );
+}
